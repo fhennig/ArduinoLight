0513925e37b24b808e7e4c059539add9552411cc
ManekenT <thohendorf@web.de>
2013-12-03 00:04:38 +0100

    Finished ambientlight structure, fixed scaling issues

--- ArduinoLight/src/gui/AmbientlightPanel.java
+++ ArduinoLight/src/gui/AmbientlightPanel.java
@@ -5,6 +5,7 @@
 
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 
@@ -14,11 +15,13 @@ import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSlider;
 import javax.swing.JSpinner;
 import javax.swing.JTable;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.ListSelectionModel;
 
 @SuppressWarnings("serial")
 public class AmbientlightPanel extends JPanel {
@@ -27,20 +30,29 @@ public class AmbientlightPanel extends JPanel {
 	
 	JPanel _screenPartPanel = new JPanel();
 	JPanel _screenOptionPanel = new JPanel();
-	JTable _screenTable = new JTable(new DefaultTableModel(4, 4));
+	JScrollPane _tablePane = new JScrollPane();
+	DefaultTableModel _tableModel = new DefaultTableModel(4, 4);
+	JTable _screenTable = new JTable();
+	JLabel _rowLabel = new JLabel("Rows: ");
 	JComboBox _rowBox = new JComboBox();
+	JLabel _colLabel = new JLabel("Coloumns: ");
 	JComboBox _colBox = new JComboBox();
+	JLabel _channelLabel = new JLabel("Channels: ");
 	JSpinner _channelSpinner = new JSpinner();
 	
-	JPanel _rgbSliderPanel = new JPanel();
-	JLabel _redLabel = new JLabel("Red: ");
-	JSlider _redSlider = new JSlider();
-	JLabel _greenLabel = new JLabel("Green: ");
-	JSlider _greenSlider = new JSlider();
-	JLabel _blueLabel = new JLabel("Blue: ");
-	JSlider _blueSlider = new JSlider();
-	JLabel _brightnessLabel = new JLabel("Brightness: ");
-	JSlider _brightnessSlider = new JSlider();
+	JPanel _rgbPanel = new JPanel();
+	JPanel _redPanel = new JPanel();
+	JPanel _greenPanel = new JPanel();
+	JPanel _bluePanel = new JPanel();
+	JPanel _brightnessPanel = new JPanel();
+	JLabel _redLabel = new JLabel("R");
+	JSlider _redSlider = new JSlider(JSlider.VERTICAL, 0, 255, 0);
+	JLabel _greenLabel = new JLabel("G");
+	JSlider _greenSlider = new JSlider(JSlider.VERTICAL, 0, 255, 0);
+	JLabel _blueLabel = new JLabel("B");
+	JSlider _blueSlider = new JSlider(JSlider.VERTICAL, 0, 255, 0);
+	JLabel _brightnessLabel = new JLabel("B");
+	JSlider _brightnessSlider = new JSlider(JSlider.VERTICAL, 0, 255, 0);
 	
 	JPanel _performancePanel = new JPanel();
 	JCheckBox _checkBox = new JCheckBox("Checkbox");		//TODO Find better names for Slider and CheckBox
@@ -52,45 +64,94 @@ public class AmbientlightPanel extends JPanel {
 	}
 	
 	private void initComponents(){
-		
-		
+		//
+		// Layouts
+		//
 		_leftPanel.setLayout(new BoxLayout(_leftPanel, BoxLayout.PAGE_AXIS));
+		_performancePanel.setLayout(new BoxLayout(_performancePanel, BoxLayout.LINE_AXIS));
+		_screenOptionPanel.setLayout(new BoxLayout(_screenOptionPanel, BoxLayout.LINE_AXIS));
+		_screenPartPanel.setLayout(new BorderLayout());
+		this.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+		_redPanel.setLayout(new BoxLayout(_redPanel, BoxLayout.PAGE_AXIS));
+		_greenPanel.setLayout(new BoxLayout(_greenPanel, BoxLayout.PAGE_AXIS));
+		_bluePanel.setLayout(new BoxLayout(_bluePanel, BoxLayout.PAGE_AXIS));
+		_brightnessPanel.setLayout(new BoxLayout(_brightnessPanel, BoxLayout.PAGE_AXIS));
+		_rgbPanel.setLayout(new BoxLayout(_rgbPanel, BoxLayout.LINE_AXIS));
+		
+		//
+		// Borders
+		//
+		_performancePanel.setBorder(new TitledBorder(null, "Performance", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.TOP));
+		_screenPartPanel.setBorder(new TitledBorder(null, "Screen Selection", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.TOP));
+		_rgbPanel.setBorder(new TitledBorder(null, "Color Correction", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.TOP));
+		
+		//
+		// Adding the Components
+		//
 		_leftPanel.add(_screenPartPanel);
 		_leftPanel.add(_performancePanel);
 		
 		_performancePanel.add(_checkBox);
-		_performancePanel.add(Box.createRigidArea(new Dimension(5, 0)));
+		_performancePanel.add(Box.createRigidArea(new Dimension(10, 0)));
 		_performancePanel.add(_slider);
-		_performancePanel.setBorder(new TitledBorder(null, "Performance", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.TOP));
-		_performancePanel.setLayout(new BoxLayout(_performancePanel, BoxLayout.LINE_AXIS));
 		
+		_screenOptionPanel.add(_rowLabel);
 		_screenOptionPanel.add(_rowBox);
+		_screenOptionPanel.add(Box.createRigidArea(new Dimension(5, 0)));
+		_screenOptionPanel.add(_colLabel);
 		_screenOptionPanel.add(_colBox);
+		_screenOptionPanel.add(Box.createHorizontalGlue());
+		_screenOptionPanel.add(_channelLabel);
 		_screenOptionPanel.add(_channelSpinner);
-		_screenOptionPanel.setLayout(new GridLayout(1, 3));
-		_screenPartPanel.add(_screenOptionPanel);
-		_screenPartPanel.add(_screenTable);
-		_screenPartPanel.setLayout(new BoxLayout(_screenPartPanel, BoxLayout.PAGE_AXIS));
-		_screenPartPanel.setBorder(new TitledBorder(null, "Screen Selection", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.TOP));
+		_screenTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		_screenTable.setRowSelectionAllowed(false);
+		_screenTable.setFillsViewportHeight(true);
+		_screenTable.setColumnSelectionAllowed(true);
+		_screenTable.setCellSelectionEnabled(true);
 		
-		_rgbSliderPanel.setLayout(new BoxLayout(_rgbSliderPanel, BoxLayout.PAGE_AXIS));
-		_rgbSliderPanel.add(Box.createVerticalGlue());
-		_rgbSliderPanel.add(_redLabel);
-		_rgbSliderPanel.add(_redSlider);
-		_rgbSliderPanel.add(Box.createVerticalGlue());
-		_rgbSliderPanel.add(_greenLabel);
-		_rgbSliderPanel.add(_greenSlider);
-		_rgbSliderPanel.add(Box.createVerticalGlue());
-		_rgbSliderPanel.add(_blueLabel);
-		_rgbSliderPanel.add(_blueSlider);
-		_rgbSliderPanel.add(Box.createVerticalGlue());
-		_rgbSliderPanel.add(_brightnessLabel);
-		_rgbSliderPanel.add(_brightnessSlider);
-		_rgbSliderPanel.add(Box.createVerticalGlue());
-		_rgbSliderPanel.setBorder(new TitledBorder(null, "Color Correction", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.TOP));
+		_screenTable.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"A", "B", "C", "D"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				false, false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		_screenTable.getColumnModel().getColumn(0).setResizable(false);
+		_screenTable.getColumnModel().getColumn(1).setResizable(false);
+		_screenTable.getColumnModel().getColumn(2).setResizable(false);
+		_screenTable.getColumnModel().getColumn(3).setResizable(false);
 		
-		this.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+		_screenPartPanel.add(_screenOptionPanel, BorderLayout.NORTH);
+		_screenPartPanel.add(_screenTable, BorderLayout.CENTER);
+		
+		_redPanel.add(_redSlider);
+		_redPanel.add(_redLabel);
+		_greenPanel.add(_greenSlider);
+		_greenPanel.add(_greenLabel);
+		_bluePanel.add(_blueSlider);
+		_bluePanel.add(_blueLabel);
+		_brightnessPanel.add(_brightnessSlider);
+		_brightnessPanel.add(_brightnessLabel);
+		_rgbPanel.add(_redPanel);
+		_rgbPanel.add(Box.createRigidArea(new Dimension(5, 0)));
+		_rgbPanel.add(_greenPanel);
+		_rgbPanel.add(Box.createRigidArea(new Dimension(5, 0)));
+		_rgbPanel.add(_bluePanel);
+		_rgbPanel.add(Box.createRigidArea(new Dimension(5, 0)));
+		_rgbPanel.add(_brightnessPanel);
+		
 		this.add(_leftPanel);
-		this.add(_rgbSliderPanel);
+		this.add(_rgbPanel);
 	}
 }
--- ArduinoLight/src/gui/Gui.java
+++ ArduinoLight/src/gui/Gui.java
@@ -25,22 +25,21 @@ import javax.swing.JComboBox;
 
 public class Gui{
 
-	private final JFrame _frame = new JFrame("Arduino Light");
+	JFrame _frame = new JFrame("Arduino Light");
 	
-	private final JTabbedPane _menuTabs = new JTabbedPane(JTabbedPane.TOP);
+	JTabbedPane _menuTabs = new JTabbedPane(JTabbedPane.TOP);
 	
-	private final AmbientlightPanel _ambientlightPanel = new AmbientlightPanel();
+	AmbientlightPanel _ambientlightPanel = new AmbientlightPanel();
 	
-	private final JPanel _soundToLightPanel = new JPanel();
+	JPanel _soundToLightPanel = new JPanel();
 	
-	private final JPanel _mainPanel = new JPanel();
-	private final JLabel _connectionSpeedLabel = new JLabel("Connection Speed: 900000");
-	private final JLabel _channelLabel = new JLabel("Channels: ");
-	private final JSpinner _channelSpinner = new JSpinner();
-	private final JLabel _lblNewLabel = new JLabel("New label");
-	@SuppressWarnings("rawtypes")
-	private final JComboBox _comboBox = new JComboBox();
-	private final JButton _connectButton = new JButton("Connect");
+	JPanel _mainPanel = new JPanel();
+	JLabel _connectionSpeedLabel = new JLabel("Connection Speed: 900000");
+	JLabel _channelLabel = new JLabel("Channels: ");
+	JSpinner _channelSpinner = new JSpinner();
+	JLabel _lblNewLabel = new JLabel("New label");
+	JComboBox _comboBox = new JComboBox();
+	JButton _connectButton = new JButton("Connect");
 		
 	public Gui(){
 		initComponents();
@@ -81,19 +80,19 @@ public class Gui{
 		_mainPanel.setLayout(new BoxLayout(_mainPanel, BoxLayout.LINE_AXIS));
 		_mainPanel.setBorder(new TitledBorder(null, "Connection Settings", TitledBorder.LEADING, TitledBorder.TOP, null, null));				
 		_mainPanel.add(_connectionSpeedLabel);
-		_mainPanel.add(Box.createVerticalGlue());
+		_mainPanel.add(Box.createHorizontalGlue());
 		_mainPanel.add(_channelLabel);
 		_mainPanel.add(_channelSpinner);
 			_channelSpinner.setModel(new SpinnerListModel(new String[] {"1", "2","3", "4"}));
-		_mainPanel.add(Box.createVerticalGlue());
+		_mainPanel.add(Box.createHorizontalGlue());
 		_mainPanel.add(_lblNewLabel);
 		_mainPanel.add(_comboBox);
-		_mainPanel.add(Box.createVerticalGlue());
+		_mainPanel.add(Box.createHorizontalGlue());
 		_mainPanel.add(_connectButton);
 		
 		_frame.add(_menuTabs, BorderLayout.CENTER);
 		_frame.add(_mainPanel, BorderLayout.SOUTH);
-		_frame.setMinimumSize(new Dimension(500, 200));
+		_frame.setMinimumSize(new Dimension(600, 450));
 		_frame.setSize(new Dimension(800, 550));
 		_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		_frame.setVisible(true);
