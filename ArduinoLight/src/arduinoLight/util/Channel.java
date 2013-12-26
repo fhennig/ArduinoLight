3d78301f4a844c078d8c653584990a3dd128e38c
Felix Hennig <3hennig@informatik.uni-hamburg.de>
2013-12-26 20:25:07 +0100

    made Channel partially thread-safe, not sure if it is completly safe.

--- ArduinoLight/src/arduinoLight/util/Channel.java
+++ ArduinoLight/src/arduinoLight/util/Channel.java
@@ -1,14 +1,18 @@
 package arduinoLight.util;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * A Channel holds a color and a name. the name should be unique!
  * @author Felix
  */
 public class Channel implements IChannel
 {
-	private static int _instances = 0; //used to generate unique names.
+	private static final AtomicInteger _instances = new AtomicInteger(0); //Used to generate Ids.
 	private final int _id;
 	private Color _color;
+	
+	
 	
 	public Channel()
 	{
@@ -18,29 +22,34 @@ public class Channel implements IChannel
 	public Channel(Color color)
 	{
 		setColor(color);
-		_id = _instances;
-		_instances++;
+		_id = _instances.getAndIncrement();
+	}
+
+	
+	
+	@Override
+	public int getId()
+	{
+		//no synchronizing needed here, because _id is final.
+		return _id;
 	}
 	
 	@Override
-	public Color getColor() {
+	public synchronized Color getColor()
+	{
 		return _color;
 	}
 
 	@Override
-	public void setColor(Color color)
+	public synchronized void setColor(Color color)
 	{
 		_color = color;
 	}
 
-	@Override
-	public int getId() {
-		return _id;
-	}
-
+	//---------- overridden from object ------------------------
 	@Override
 	public int hashCode() {
-		return _id;
+		return _id * 137;
 	}
 
 	@Override
@@ -61,5 +70,11 @@ public class Channel implements IChannel
 			return false;
 		}
 		return true;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return _id + ": " + _color.toString();
 	}
 }
