19b978bfcd6abedc4c21d7db0f7a7ee4c9be826c
ManekenT <thohendorf@web.de>
2014-02-27 19:30:44 +0100

    - Fixed Folder Structure for fxml Files (now located in gui package). - Fixed loading of fxml files. - Added min/max window size and shutdown handler.

--- ArduinoLight/src/arduinoLight/ArduinoLight.java
+++ ArduinoLight/src/arduinoLight/ArduinoLight.java
@@ -3,27 +3,22 @@ package arduinoLight;
 import gnu.io.CommPortIdentifier;
 import gnu.io.PortInUseException;
 
+import java.io.IOException;
 import java.util.*;
 
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
-import javax.swing.SwingUtilities;
-
+import javafx.stage.WindowEvent;
 import arduinoLight.arduino.SerialConnection;
 import arduinoLight.arduino.amblone.AmbloneTransmission;
 import arduinoLight.channel.Channel;
 import arduinoLight.channelholder.ambientlight.Ambientlight;
 import arduinoLight.channelholder.ambientlight.Areaselection;
-import arduinoLight.gui.ChannelModifyPanel;
-import arduinoLight.gui.Gui;
-import arduinoLight.gui.TabPanel;
-import arduinoLight.gui.ambientLight.AmbientlightPanel;
-import arduinoLight.gui.connectionPanel.SerialConnectionPanel;
 import arduinoLight.model.Model;
 import arduinoLight.util.Color;
 
@@ -36,23 +31,39 @@ public class ArduinoLight extends Application
 	
 	@Override
 	public void start(Stage stage) throws Exception {
-		stage.setTitle("ArduinoLight");
+		initGui(stage);
 		
-		Parent root = FXMLLoader.load(getClass().getResource("res/MainPane.fxml"));
-		Scene scene = new Scene(root,400,400);
-		stage.setScene(scene);
-		stage.show();
+		SerialConnection connection = new SerialConnection();
+		AmbloneTransmission amblone = new AmbloneTransmission();
+		Ambientlight ambientlight = Model.getInstance().getAmbientlight();
 		
 		//Add some Channels for testing purposes:
-		//Model.getInstance().getChannelFactory().newChannel("RED").setColor(Color.RED);
-		//Model.getInstance().getChannelFactory().newChannel("GREEN").setColor(Color.GREEN);
-		//Model.getInstance().getChannelFactory().newChannel("BLUE").setColor(Color.BLUE);
+		Model.getInstance().getChannelFactory().newChannel("RED").setColor(Color.RED);
+		Model.getInstance().getChannelFactory().newChannel("GREEN").setColor(Color.GREEN);
+		Model.getInstance().getChannelFactory().newChannel("BLUE").setColor(Color.BLUE);
 	}
 	
-	//public static void main(String[] args)
-	//{		
-	//	launch(args);
-	//}
+	public static void main(String[] args)
+	{		
+		launch(args);
+	}
+	
+	private void initGui(Stage stage) throws IOException{
+		stage.setTitle("ArduinoLight");
+		Parent root;
+		root = FXMLLoader.load(getClass().getResource("gui/fxml/MainPane.fxml"));
+		Scene scene = new Scene(root,600,400);
+		stage.setScene(scene);
+		stage.setMinWidth(600);
+		stage.setMinHeight(400);
+		stage.setOnCloseRequest(new EventHandler<WindowEvent>(){
+			@Override
+			public void handle(WindowEvent event) {
+				System.exit(0);
+			}
+		});
+		stage.show();
+	}
 		
 	/**	//Initialize GUI in Swing event dispatch thread
 		SwingUtilities.invokeLater(new Runnable()
