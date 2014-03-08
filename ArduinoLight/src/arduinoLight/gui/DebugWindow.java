f321a287afbe9e6490d8481673db4d012b4fc692
Felix Hennig <3hennig@informatik.uni-hamburg.de>
2014-03-08 17:27:36 +0100

    ~

---
+++ ArduinoLight/src/arduinoLight/gui/DebugWindow.java
@@ -0,0 +1,18 @@
+package arduinoLight.gui;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class DebugWindow extends JFrame
+{
+	public DebugWindow()
+	{
+		initComponents();
+	}
+	
+	private void initComponents()
+	{
+		
+	}
+}
+
